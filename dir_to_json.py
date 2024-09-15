f9b72941fd360a8d6bba3e5c1b8f940517ca24cf
HlexNC <hlexhelftd@gmail.com>
2024-09-15 14:06:52 +0200

    reface: dir to json code blocks

--- dir_to_json.py
+++ dir_to_json.py
@@ -7,7 +7,7 @@ def read_file(file_path):
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
             content = file.read()
-            return content.replace('`', '\\`')
+            return content.replace('`', '~')
     except UnicodeDecodeError:
         return None
 
